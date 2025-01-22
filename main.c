80c7b4afedf475efe6584f693fa4d26f2342dd0a
Tello <perezgarrote.tello@gmail.com>
2025-01-22 20:32:02 +0100

    Salen rayos del circulo pero no hay deteccion de hit

--- main.c
+++ main.c
@@ -6,12 +6,21 @@
 #define HEIGHT 600
 #define COLOR_WHITE 0xffffffff
 #define COLOR_BLACK 0x00000000
+#define COLOR_GRAY 0xefefefef
+#define RAYS_NUMBER 100
 
 struct Circle
 {
 	double x;
 	double y;
 	double radius;
+};
+
+struct Ray
+{
+	double x_start,y_start;
+	double angle;
+	double x_end,y_end;
 };
 
 void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 color)
@@ -29,7 +38,44 @@ void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 color)
 				SDL_Rect pixel = (SDL_Rect){x,y,1,1};
 				SDL_FillRect(surface, &pixel, color);
 			}
+		}
+	}
+}
 
+void generate_rays(struct Circle circle ,struct Ray rays[RAYS_NUMBER])
+{
+	for(int i=0;i<RAYS_NUMBER;i++)
+	{
+		double angle = ((double) i/ RAYS_NUMBER)*2*M_PI;
+		struct Ray ray = {circle.x,circle.y,angle};
+		rays[i] = ray;
+	}	
+}
+
+void FillRays(SDL_Surface* surface, struct Ray rays[RAYS_NUMBER], Uint32 color)
+{
+	for(int i=0;i<RAYS_NUMBER;i++)
+	{
+		struct Ray ray = rays[i];
+		
+		int end_of_screen = 0;
+		int object_hit = 0;
+		
+		double step = 1;
+		double x_draw = ray.x_start;
+		double y_draw = ray.y_start;
+		while (!end_of_screen && !object_hit)
+		{
+			x_draw += step*cos(ray.angle);
+			y_draw += step*sin(ray.angle);
+
+			SDL_Rect pixel = (SDL_Rect){x_draw,y_draw,1,1};
+			SDL_FillRect(surface,&pixel,color);
+			
+			if(x_draw < 0 || x_draw > WIDTH)
+				end_of_screen = 1;
+			if(y_draw < 0 || y_draw > HEIGHT)
+				end_of_screen = 1;
 		}
 	}
 }
@@ -42,7 +88,11 @@ int main()
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
 	struct Circle circle = {200,200,80};
+	struct Circle shadow_circle = {650,300,140};
 	SDL_Rect erase_rect = {0,0,WIDTH,HEIGHT};	
+	
+	struct Ray rays[RAYS_NUMBER];
+	generate_rays(circle,rays);
 
 	int simulation_running = 1;
 	SDL_Event event;
@@ -59,11 +109,15 @@ int main()
 			{
 				circle.x = event.motion.x;
 				circle.y = event.motion.y;
+				generate_rays(circle,rays);
 			}
 		}
 		// Borramos la pantalla entera(rect) y ponemos el circulo donde el mouse
 		SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
 		FillCircle(surface,circle,COLOR_WHITE);
+		
+		FillCircle(surface,shadow_circle,COLOR_WHITE);
+		FillRays(surface,rays,COLOR_GRAY);
 
 		SDL_UpdateWindowSurface(window);
 		SDL_Delay(10);
